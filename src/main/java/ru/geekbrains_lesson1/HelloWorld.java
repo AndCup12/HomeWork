8852bade388b741a4cabdd5243748a4684fb6836
AndCup12 <andrejprasolov93239@gmail.com>
2021-03-20 16:25:23 +0300

    Initial commit

---
+++ src/main/java/ru/geekbrains_lesson1/HelloWorld.java
@@ -0,0 +1,24 @@
+package ru.geekbrains_lesson1;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        variablesExample();
+
+
+    }
+
+    private static void variablesExample() {
+        byte byteVariable = 100;
+        byte b2;
+        b2 = 50;
+//       var a = 10;
+        short shortValue = 500;
+        int intValue = 1000;
+        long longValue = 3000;
+    }
+
+    static void printer() {
+    }
+}
+
